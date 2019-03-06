3e74e90f620b36a72dc85bc9fbbefb402a069503
starkblast <chazzykaku@gmail.com>
2019-03-06 09:27:38 +0200

    added source info

--- client/src/App.js
+++ client/src/App.js
@@ -40,14 +40,16 @@ class QuoteMachine extends React.Component {
 
   render() {
     return (
-      <div id="quote-box">
-        <h3 id="text">{this.state.currentQuote}</h3>
-        <h5 id="author">- {this.state.currentAuthor}</h5>
-        <div className="button-box">
-           <a id="tweet-quote" target="_blank" rel="noopener noreferrer" href={"https://twitter.com/intent/tweet?text="+this.state.currentQuote}><button id="tweet-button"><FontAwesomeIcon icon={faTwitter} /> Tweet</button></a>
-          <button onClick={this.fetchQuote} id="new-quote">New quote ></button>
-        </div>
-
+      <div id="container">
+          <div id="quote-box">
+            <h3 id="text">{this.state.currentQuote}</h3>
+            <h5 id="author">- {this.state.currentAuthor}</h5>
+            <div className="button-box">
+               <a id="tweet-quote" target="_blank" rel="noopener noreferrer" href={"https://twitter.com/intent/tweet?text="+this.state.currentQuote}><button id="tweet-button"><FontAwesomeIcon icon={faTwitter} /> Tweet</button></a>
+              <button onClick={this.fetchQuote} id="new-quote">New quote ></button>
+            </div>
+          </div>
+          <h6 id="footer">markov chain implementation by <a target="_blank" rel="noopener noreferrer" href="https://github.com/jsvine/markovify">jsvine</a> / original quote input from <a target="_blank" href="http://wisdomquotes.com/inspirational-quotes" rel="noopener noreferrer">wisdomquotes </a></h6>
       </div>
     );
 
